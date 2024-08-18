12ded535a47c3f855edc225145ea925198c90e51
jzdevnet <jtz308@hotmail.com>
2024-08-18 00:52:19 -0700

    header-collapse leftover removed

--- src/scripts/header-collapse.js
+++ src/scripts/header-collapse.js
@@ -1,9 +1,4 @@
 document.querySelector('.navbar-toggle').addEventListener('click', (e) => {
-	// if(!document.querySelector('.navbar-collapse').classList.contains('open')){
-	//     let linkHeight = document.querySelector('.navbar-collapse ul').offsetHeight;
-	//     document.querySelector('.navbar-collapse').style.height = `${linkHeight}px`;
-	// }
-	// else{ document.querySelector('.navbar-collapse').style.height = `0px`; }
 	e.target.classList.toggle('open');
 	document.querySelector('.navbar-collapse').classList.toggle('open');
 	document.querySelector('body').classList.toggle('navbar-open');
